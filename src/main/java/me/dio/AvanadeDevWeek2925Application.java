ca96104365e27672ea93925fe0ff9acbf926d5d9
Alexdevsoft <alexhavilla2022@gmail.com>
2025-03-14 19:59:14 -0300

    spring

---
+++ src/main/java/me/dio/AvanadeDevWeek2925Application.java
@@ -0,0 +1,13 @@
+package me.dio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvanadeDevWeek2925Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvanadeDevWeek2925Application.class, args);
+	}
+
+}
