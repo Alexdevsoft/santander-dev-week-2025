680c45296bc74d9794f989a7a6e27a0377cae3ab
Alexdevsoft <alexhavilla2022@gmail.com>
2025-03-15 21:33:44 -0300

    Implementação da camada de dominio

--- src/main/java/me/dio/Application.java
+++ src/main/java/me/dio/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AvanadeDevWeek2925Application {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AvanadeDevWeek2925Application.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
